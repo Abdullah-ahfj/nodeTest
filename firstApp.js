3a4422dcf3c1d4bc22f66d28960b6da81f45e816
Abdullah-ahfj <abdullahahfj@gmail.com>
2024-09-20 09:25:39 +0530

    init

---
+++ firstApp.js
@@ -0,0 +1,21 @@
+// let car = {
+//     name : "BMW",
+//     color : "blue",
+
+//     carInfo () {
+//         console.log("This is a " + this.name +", and it is " + this.color + " car");
+       
+    
+//     }
+// }
+
+let colors = ["White", "Blue", "Green", "Black", "Red", "Yellow", "White", "White", "Black"];
+
+const [color1,color2,color3,color4,color5,color6,color7,color8,color9] = colors;
+
+console.log(color1);
+console.log(color3);
+console.log(color1,color3,color6);
+
+
+
